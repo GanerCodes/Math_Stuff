26d3be8956c5ba0e2f86ca35e5bc1dcd9ae5d266
GanerCodes <ganer@ganer.xyz>
2023-02-06 10:04:30 -0600

    xd

--- row_es/main.py
+++ row_es/main.py
@@ -1,11 +1,14 @@
+from itertools import product
+from more_itertools import grouper
 from fractions import Fraction
 
-mat = [ [3, 2, 4],
-        [5, 6, 1],
-        [1, 4, 9] ]
+mat = [ [3, 2, 4, 3],
+        [5, 6, 1, 5],
+        [1, 4, 9, 2] ]
+l = len(mat)
 
 mat = [[Fraction(o) for o in i] for i in mat]
-
+ident = [*grouper([Fraction(i==o) for i,o in product(range(l), repeat=2)], l)]
 def mul_sca(a, s):
     return [i*s for i in a]
 def add_arr(a, o, m=1):
@@ -14,9 +17,28 @@ def add_arr(a, o, m=1):
 def p(a=mat):
     for i in a:
         for o in i:
-            print("%s/%s" % o.as_integer_ratio(), end=' ')
+            print("%s/%s" % o.as_integer_ratio(), end='\t')
         print()
 
-mat[-1] = add_arr(mat[-1], mat[-2], -(mat[-1][0] / mat[-2][0]))
-mat[-2] = add_arr(mat[-2], mat[-1], -(mat[-2][1] / mat[-1][1]))
-p()
+def op1(mat, ident, c, r):
+    j = -mat[c][r]/mat[r][r]
+    mat[c] = add_arr(mat[c], mat[r], j)
+    ident[c] = add_arr(ident[c], ident[r], j)
+def op2(mat, ident, r):
+    j = 1/mat[r][r]
+    mat[r] = mul_sca(mat[r], j)
+    ident[r] = mul_sca(ident[r], j)
+
+for r in range(l):
+    row = mat[r]
+    m = row[r]
+    for c in range(l):
+        if c == r:
+            continue
+        op1(mat, ident, c, r)
+    op2(mat, ident, r)
+
+print("Row Echelon form:")
+p(mat)
+print("Matrix inverse:")
+p(ident)
