cc9b217c7131e341396e9eb0116d0fdb2a4f3e39
GanerCodes <ganer@ganer.xyz>
2023-02-09 12:48:10 -0600

    xd

--- row_es/main.py
+++ row_es/main.py
@@ -2,16 +2,14 @@ from itertools import product
 from more_itertools import grouper
 from fractions import Fraction
 
-mat = [ [-1,-3,-3],
-        [ 2, 6, 1],
-        [ 3, 8, 3] ]
+mat = [ [4, 3], [3, 1] ]
 l = len(mat)
 
 mat = [[Fraction(o) for o in i] for i in mat]
 ident = [*grouper([Fraction(i==o) for i,o in product(range(l), repeat=2)], l)]
 
-mat[1],mat[2]=mat[2],mat[1]
-ident[1],ident[2]=ident[2],ident[1]
+# mat[1],mat[2]=mat[2],mat[1]
+# ident[1],ident[2]=ident[2],ident[1]
 
 def dot(A, B):
     return sum(a*b for a,b in zip(A,B))
@@ -19,8 +17,21 @@ def dot(A, B):
 def mat_mul(a, b):
     return [[dot(a[y], (v[x] for v in b)) for x in range(len(b[0]))] for y in range(len(a))]
 
+def det(A):
+    if len(A) == 1: return A[0][0]
+    r = 0
+    for i, v in enumerate(A[0]):
+        r += (1-2*(i%2)) * v * det([[c for n, c in enumerate(r) if n != i] for r in A[1:]])
+    return r
+
+print(det([[2,0,0,1],
+           [0,1,0,0],
+           [1,6,2,0],
+           [1,1,-2,3]]))
+
 # print(mat_mul([[2,6,-2],[1,3,4]],[[2,1],[4,6]]))
-print(mat_mul([[1,1],[0,0]],[[-1,-1],[1,1]]))
+# print(mat_mul([[1,1],[0,0]],[[-1,-1],[1,1]]))
+print(mat_mul([[2,-1],[-5,3]],[[1,2],[3,4]]))
 
 def mul_sca(a, s):
     return [i*s for i in a]
@@ -49,8 +60,6 @@ for r in range(l):
         if c == r:
             continue
         op1(mat, ident, c, r)
-        p(mat)
-        print()
     op2(mat, ident, r)
 
 print("Row Echelon form:")
